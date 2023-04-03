25b02aed38caef79677b10778863ba6b65ced579
Aswinkumaresan <aswinabi2000@gmail.com>
2023-04-03 08:05:46 +0530

    updated task

--- my-app/src/Form/FormTask.js
+++ my-app/src/Form/FormTask.js
@@ -1,38 +1,63 @@
 import React, { useState } from "react";
 
 const FormTask = () => {
+    const[updateName,SetUpdatedName]=useState("");
+    const[description,SetUpdatedDescription]=useState("");
+    const[completed,Setcompletion]=useState();
+    const[arrayvalue,setArray]=useState([]);
+    const[showerror,setError]=useState(false);
+   
 
- const[username,setname]=useState("")   
- const[userdetail,setdetail]=useState("")   
- const[iscomplete,setcomplete]=useState("")   
- const[newarrdata,setnewarrdata]=useState("")   
- const[submit,setsubmit]=useState("")
-
- const ChangeName=(e)=>{
-    console.log("e",e.target.value);
-    setname(e.target.value)
- }
- const ChangeDetails=(e)=>{
-    console.log("e",e.target.value);
-    setdetail(e.target.value)
- }
- const onCheck=(e)=>{
-    console.log("e",e.target.value);
-    setcomplete(e.target.value)
- }
+    const handleUpdatedName=(e)=>{
+        console.log("tasktname",e.target.value)
+        SetUpdatedName(e.target.value)
+    }
+    const handleDesvalue=(e)=>{
+        console.log("description",e.target.value)
+        SetUpdatedDescription(e.target.value)
+    }
+    const handlecheckbox=(e)=>{
+        console.log("completed",e.target.checked)
+        Setcompletion(e.target.checked ? "YES":"NO")
+    }
+    const handlesubmit=(e)=>{
+        e.preventDefault();
+        
+        setError(true);
+        if(updateName==="" || description==="")return;
+       
+        console.log(updateName,description,completed)
+       pusharr();
+       
+       
+    }
+      const pusharr=()=>{
+        setArray([...arrayvalue, {name:updateName,des:description,iscomplete:completed}]);
+        //console.log(arrayvalue)
+    }
 
   return <div>
 
     <h1>Form-Task</h1>
-    <form>
-        {/* <label for="userName">Enter User Name</label> */}
-        <input type="text" name="userName" placeholder="Enter User-Name" onChange={ChangeName}/>
-        <input type="text" name="userDescription" placeholder="Enter User-Details" onChange={ChangeDetails}/>
-        <input type="checkbox" onChange={onCheck}>
-        
-
-        </input>
-    </form>
+     <form onSubmit={handlesubmit}>
+            <label >updateName:</label>
+            <input type="text" placeholder="updateName"  onChange={handleUpdatedName}/><br/>
+            {updateName === "" && showerror &&<p>Task name is required</p>}
+            <label >Description:</label>
+            <input type="text" placeholder="description"  onChange={handleDesvalue}/><br/>
+            {description === ""&& showerror &&<p>Description is required</p>}
+            <label >isCompleted:</label>
+            <input type="checkbox" onChange={handlecheckbox}/>
+            <input type="submit"/>
+        </form>
+        <ul>Task List:
+            
+            {arrayvalue.map((item,index)=>(
+                <li key={index}>updateName:{item.name} <br/> Description:{item.des} <br/> isCompleted:{item.iscomplete}</li>
+               
+               
+            ))}
+        </ul>
   </div>;
 };
 
